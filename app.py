33e89f5675b65a1586a34bd62994ae49730946e4
Roshan Gunarathna <roshanrnofficial@gmail.com>
2023-11-13 23:47:44 +0530

    refreshh

--- app.py
+++ app.py
@@ -15,4 +15,7 @@ def pred():
     return jsonify(message)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=False,host='0.0.0.0')
+
+# if __name__ == "__main__":
+#     app.run(debug=false)
